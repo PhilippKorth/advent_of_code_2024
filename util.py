495be457c953e952c2a3ddd2dbdee63d2f6997b8
Philipp Korth <pkorth@dspace.de>
2024-12-15 13:12:18 +0100

    Adds another functtion to help with day 5

--- util.py
+++ util.py
@@ -1,5 +1,15 @@
+from itertools import groupby
 
 def read_file(file_path):
     """Gets the content from the given file"""
     with open(file_path, 'r') as input_file:
         return input_file.read()
+
+
+def read_sperated_file(file_path, split_on_seperator=''):
+    """
+    If the input itself is seperated by something, split int into sets of inputs
+    """
+    content = read_file(file_path)
+    return [list(v) for k, v in groupby(content.splitlines() , key=split_on_seperator.__eq__) if not k]
+    
